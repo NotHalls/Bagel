0d3194f31d439603042aec2b706396bf33920a16
NotHalls <notharshith74@gmail.com>
2024-11-06 02:38:17 +0530

    OH Yea, no more glfw errors while quiting either

--- src/Main.cpp
+++ src/Main.cpp
@@ -8,8 +8,4 @@ int main()
     Application* App = CreateApp();
 
     App->Run();
-
-    // @FIXME: im getting glfw errors while closing the app
-    // i know that im getting those because glfw is probably not being closed when i close my app
-    // and im trying to access glfw functions. like Input or GetGlfwTime
 }
